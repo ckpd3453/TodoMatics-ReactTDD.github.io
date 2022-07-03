b21042a1e8adf977bcf2e0b8bf4415ad4290db3c
Chandra <chandrakantprasad25@gmail.com>
2022-07-03 20:26:02 +0530

    Add Test-Case in react for TodoMatics App

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import ListTodo from './components/ListTodo';
+import TextField from './components/TextField';
+import FilterButton from './components/filterButtons';
+
+function App(props) {
+
+  const [data, setData] = useState(props.data);
+  const [filter,setFilter] = useState("All");
+
+  const Filters = {
+    All:()=>true,
+    Completed:(task)=>task.completed,
+    Todo: (task)=>!task.completed
+  };
+
+  const FilterNames = Object.keys(Filters);
+
+  function handleFilter(filter_name){
+    setFilter(filter_name);
+  }
+
+  const FilterButtons = FilterNames.map((task)=>
+     <FilterButton name={task} key={task} currentFilter={filter} set_filter={handleFilter}/>
+  );
+
+  function addNote(newName){
+    const newNote = {id:"todo-"+ nanoid(), name:newName, completed: false};
+    setData([...data, newNote]);
+    setTimeout(() => {
+      alert("New note created")
+    }, 100); 
+  }
+
+  function changeCompleted(id){
+    const modTasks = data.map((dataItem)=>{
+      if(dataItem.id==id){
+        return {...dataItem,completed:!dataItem.completed};
+      }
+      return dataItem;
+    });
+    setData(modTasks);
+  }
+
+  function deleteNotes(id){
+    const modTasks = data.filter((dataItem)=> dataItem.id!=id);
+    setData(modTasks);
+  }
+
+  function modifyNote(id,value){
+    const modTasks = data.map((dataItem)=>{
+      if(dataItem.id==id){
+        return {...dataItem,name:value};
+      }
+      return dataItem;
+    });
+    setData(modTasks);
+  }
+
+  const list = (data).filter(Filters[filter]).map((dataItem)=>{
+  return (<ListTodo listItem={dataItem.name} key={dataItem.id} id={dataItem.id} completed={dataItem.completed} 
+    toggleButton={changeCompleted} deleteNotes={deleteNotes} modifyNote={modifyNote} />)
+  });
+
+  let lengthText = list.length;
+  if(lengthText==1){
+    lengthText += " task available";
+  }
+  else{
+    lengthText += " tasks available";
+  }
+
+
+  return (
+    <div className='body-container'>
+    <h1 data-testid="Header"> Todo_Matic</h1>
+    <hr className='head-line'/>
+    <h2><u> Add a new note</u></h2>
+    <div className='input-div'><TextField setData={addNote} /></div>
+    <div className="filterbutton btn-group">
+    {FilterButtons}
+    </div>
+    <div className='length-div'>
+    <label htmlFor='list' data-testid="noOfTasks"> {lengthText} </label>
+    </div>
+    <div className='list-div'>
+    <ul data-testid="list-todo" id="list">
+      {list}
+    </ul>
+    </div>
+    </div>
+  );
+}
+
+export default App;
